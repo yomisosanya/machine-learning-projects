eca54d8b29ca283605d4d38e1bef7c03ea9e19f3
yomisosanya <98689579+yomisosanya@users.noreply.github.com>
2025-09-23 21:51:54 -0400

    PCA_feature_selection.py done

--- supervised_learning/src/PCA_feature_selection.py
+++ supervised_learning/src/PCA_feature_selection.py
@@ -3,7 +3,12 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import cross_val_predict, KFold
 from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+
+# import sys
 
 
 uri = Path("../../res/iris.csv")
@@ -25,16 +30,16 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 pca = PCA(n_components=4)
-pca.fit(X_scaled)
+X_pca = pca.fit_transform(X_scaled)
 
 eigenvectors = pca.components_
 eigenvalues = pca.explained_variance_
 
-print("The eigenvalues are {}".format(eigenvalues), file=output_txt)
+print("PCA Feature Selection\n", file=output_txt)
+
+print("\nThe eigenvalues are {}\n".format(eigenvalues), file=output_txt)
 print("The eigenvectors are:", file=output_txt)
 print(eigenvectors, file=output_txt)
-
-output_txt.close()
 
 pca_range = np.arange(1, len(eigenvalues) + 1)
 
@@ -46,3 +51,24 @@ plt.ylabel("Eigenvalues")
 plt.grid(True)
 plt.savefig(Path("../build/scree.png"))
 plt.close()
+
+
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+
+y_pred = cross_val_predict(
+    estimator=DecisionTreeClassifier(),
+    cv=KFold(n_splits=5, shuffle=True, random_state=42),
+    X=X_pca,
+    y=y,
+)
+
+cm = confusion_matrix(y, y_pred=y_pred)
+acc = accuracy_score(y, y_pred=y_pred)
+
+print("\nThe confusion matrix is:", file=output_txt)
+print("{}\n".format(cm), file=output_txt)
+print("The accuracy score is: {}\n".format(acc), file=output_txt)
+
+
+output_txt.close()
---
+++ supervised_learning/src/kmeans.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def kmeans(points, n_clusters, thresholds=0.01):
+    """ """
+
+
+if __name__ == "__main__":
+    pass
