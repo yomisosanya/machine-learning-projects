2002c7bd06c4747c909b60ecfd431fa1838edcf3
yomisosanya <98689579+yomisosanya@users.noreply.github.com>
2025-10-28 23:40:30 -0400

    cnn model added

--- computer_vision/src/cnn.py
+++ computer_vision/src/cnn.py
@@ -1,11 +1,13 @@
+from torch import nn
 from torchvision import datasets 
 from torchvision import transforms
+
 
 dataset_dir = '../../res'
 
 transform = transforms.Compose(
     [transforms.To_Tensor(),
-    transforme.Normalize((0.1307,), (0.3081))]
+    transforms.Normalize((0.1307,), (0.3081))]
 )
 
 dataset_train = datasets(
@@ -21,3 +23,19 @@ dataset_test = datasets(
     download = False
     transform = transform
 )
+
+
+class CNN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Sequential()
+        self.layer2 = nn.Sequential()
+        self.fully_conn = nn.Linear()
+
+    def forward(self, input):
+        output = self.layer1(input)
+        output = self.layer2(output)
+        output = output.reshape()
+        output = self.fully_conn(output)
+        return output
